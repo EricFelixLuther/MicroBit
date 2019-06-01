3c502cf2a43380597aac1f401bad8dafcb69e4a0
EricFelixLuther <EricFelixLuther@users.noreply.github.com>
2019-06-01 23:13:45 +0200

    Tony Stark Heart v.1.6
    Tony Stark's heart now have three modes, two first can switch colors.

--- tony_stark_heart.py
+++ tony_stark_heart.py
@@ -3,131 +3,156 @@ import neopixel
 import random
 import music
 
-
-class Color():
+class Rainbow():
     def __init__(self):
         self.r = 0
         self.g = 0
         self.b = 0
+        self.ri = 0
+        self.gi = 0
+        self.bi = 0
 
-    def color(self):
-        return (self.r, self.g, self.b)
+    def increment(self):
+        self.r += self.ri
+        self.g += self.gi
+        self.b += self.bi
 
     def __call__(self):
-        yield self.color()
+        # if red is at max: remove blue
+        # if blue is at zero: add green
+        # if green is at max: remove red
+        # if red is at zero: add blue
+        # if blue as at max: remove green
+        # if green is at zero: add red
+        if self.r == 0 and self.g == 0 and self.b == 0:
+            # Start state. Begin adding red
+            self.ri = 1
+            self.gi = 0
+            self.bi = 0
+        if self.r == 255 and self.g == 0 and self.b == 0:
+            # Red is at max. Start adding green
+            self.ri = 0
+            self.gi = 1
+            self.bi = 0
+        if self.r == 0 and self.g == 255 and self.b == 0:
+            # Green is at max. Start adding blue
+            self.ri = 0
+            self.gi = 0
+            self.bi = 1
+        if self.r == 0 and self.g == 0 and self.b == 255:
+            # Blue is at max, start adding red
+            self.ri = 1
+            self.gi = 0
+            self.bi = 0
+        if self.r == 255 and self.g == 255 and self.b == 0:
+            # Red and green is yellow. Start removing red
+            self.ri = -1
+            self.gi = 0
+            self.bi = 0
+        if self.r == 255 and self.g == 0 and self.b == 255:
+            # Red and blue is purple. Start removing blue
+            self.ri = 0
+            self.gi = 0
+            self.bi = -1
+        if self.r == 0 and self.g == 255 and self.b == 255:
+            # Green and blue is cyan. Start removing green
+            self.ri = 0
+            self.gi = -1
+            self.bi = 0
+        if self.r == 255 and self.g == 255 and self.b == 255:
+            # This situation should not occur
+            raise Exception("White color achieved")
 
-class White(Color):
-    def __init__(self):
-        self.r = 255
-        self.g = 255
-        self.b = 255
-
-class DimWhite(Color):
-    def __init__(self):
-        self.r = 100
-        self.g = 100
-        self.b = 100
-
-class Red(Color):
-    def __init__(self):
-        self.r = 255
-        self.g = 0
-        self.b = 0
-        
-class Blue(Color):
-    def __init__(self):
-        self.r = 0
-        self.g = 0
-        self.b = 255
-
-class Purple(Color):
-    def __init__(self):
-        self.r = 255
-        self.g = 0
-        self.b = 255
-        
-class Rainbow(Color):
-    def __call__(self):
-        while self.r < 255:     # add red
-            self.r += 1
-            yield self.color()
-        while self.b > 0:       # remove blue
-            self.b -= 1
-            yield self.color()
-        while self.g < 255:     # add green
-            self.g += 1
-            yield self.color()
-        while self.r > 0:       # remove red
-            self.r -= 1
-            yield self.color()
-        while self.b < 255:     # add blue
-            self.b += 1
-            yield self.color()
-        while self.g > 0:       # remove green
-            self.g -= 1
-            yield self.color()
-
-class Colors(): 
-    def __init__(self):
-        self.colors = (White, DimWhite, Red, Blue, Purple, Rainbow)
-        self.i = -1
-
-    def get_next_color(self):
-        self.i += 1
-        if self.i >= len(self.colors):
-            self.i = 0
-        return self.colors[self.i]()
-
-class Index():
-    def __init__(self):
-        self.i = -1
-    
-    def __call__(self):
-        self.i += 1
-        if self.i >= 24:
-            self.i = 0
-        return self.i
-        
-def heartbeat_sound():
-    music.play(["C1:1"], wait=False)
+        self.increment()
+        return [self.r, self.g, self.b]
 
 def heartbeat(led):
-    if led in (7, 15, 23):
+    if led in (0, 2, 8, 10, 16, 18):
         display.show(Image.HEART_SMALL, wait=False)
-        heartbeat_sound()
+        music.play(["C1:1"], wait=False)
     else:
         display.show(Image.HEART, wait=False)
-        if led in (1, 10, 18):
-            heartbeat_sound()
     
 np = neopixel.NeoPixel(pin2, 24)
-i = Index()
-mode = 0
-#colors = Colors()
-#color = colors.get_next_color() 
-#LEDS = range(24)
+l = 0
+colors = (
+    [255, 255, 255],  # white
+    [255, 0, 0],  # red
+    [0, 255, 0],  # green
+    [0, 0, 255],  # blue
+    [255, 0, 255],  # purple
+    [255, 80, 0]  # orange
+)
+color_i = 0
+color = colors[color_i]
+rainbow = Rainbow()
+leds = range(24)
 
-while mode == 0:
-    led = i()
-    #if button_a.get_presses():
-    #    mode += 1
-    #    if mode >= 4:
-    #        mode = 1
-    #if button_b.get_presses():
-    #    color = colors.get_next_color()
-    #c = list(color())
+i = 0
+ii = 1
 
-    heartbeat(led)
+while True:
+    while True:
+        if button_a.get_presses():
+            break
+        if button_b.get_presses():
+            color_i += 1
+            if color_i >= len(colors):
+                color_i = 0
+            color = colors[color_i]
+    
+        heartbeat(l)
 
-    for other_leds in (0, -1, -2, -3, -4):
-        other_led = led + other_leds
-        color_for_led = [max(c - 51 * abs(other_leds), 0) for c in [255, 0, 255]]
-        np[other_led] = color_for_led
-    np.show()
-    sleep(100)
+        for other_led in (0, 1, 2, 3, 4, 5):
+            np[l - other_led] = [max(c - int(c * 0.2 * other_led) - 1, 0) for c in color]
+        np.show()
+        sleep(100)
+        l += 1
+        if l >= 24:
+            l = 0
+
+    while True:
+        if button_a.get_presses():
+            break
+        if button_b.get_presses():
+            color_i += 1
+            if color_i >= len(colors):
+                color_i = 0
+            color = colors[color_i]
+    
+        #heartbeat(l)
+
+        for led in leds:
+            try:
+                np[led] = [int(c * i / 100) for c in color]
+            except:
+                display.scroll(str(i))
+        np.show()
+        sleep(20)
+        l += 1
+        if l >= 24:
+            l = 0
+
+        if i <= 0:
+            i = 0
+            ii = 1
+        elif i >= 100:
+            i = 100
+            ii = -1
+        i += ii
+    
     np.clear()
-    #elif mode == 1:
-    #    np[i()] = c
-    #    np.show()
-    #elif mode == 2:
-    #    pass
+    color = Rainbow()
+    while True:
+        if button_a.get_presses():
+            break
+        
+        heartbeat(l)
+
+        for other_led in (0, 1, 2, 3, 4, 5):
+            np[l - other_led] = [max(c - int(c * 0.2 * other_led) - 1, 0) for c in color()]
+        np.show()
+        sleep(100)
+        l += 1
+        if l >= 24:
+            l = 0
